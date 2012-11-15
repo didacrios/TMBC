0b3c699210882f9f82f817c4e069604090ce7004
Dídac Rios <didac@niorcs.com>
2012-11-15 09:55:36 +0100

    Afegim funcio ts_sel

--- paginador.class.php
+++ paginador.class.php
@@ -1,245 +1,253 @@
 <?php
-/* =============================================================================
-                                                      __                    
-                           __                        /\ \                   
- _____      __        __  /\_\     ___       __      \_\ \     ___    _ __  
-/\ '__`\  /'__`\    /'_ `\\/\ \  /' _ `\   /'__`\    /'_` \   / __`\ /\`'__\
-\ \ \L\ \/\ \L\.\_ /\ \L\ \\ \ \ /\ \/\ \ /\ \L\.\_ /\ \L\ \ /\ \L\ \\ \ \/ 
- \ \ ,__/\ \__/.\_\\ \____ \\ \_\\ \_\ \_\\ \__/.\_\\ \___,_\\ \____/ \ \_\ 
-  \ \ \/  \/__/\/_/ \/___L\ \\/_/ \/_/\/_/ \/__/\/_/ \/__,_ / \/___/   \/_/ 
-   \ \_\              /\____/                                               
-    \/_/              \_/__/                                
+	/* =============================================================================
+	                                                      __                    
+	                           __                        /\ \                   
+	 _____      __        __  /\_\     ___       __      \_\ \     ___    _ __  
+	/\ '__`\  /'__`\    /'_ `\\/\ \  /' _ `\   /'__`\    /'_` \   / __`\ /\`'__\
+	\ \ \L\ \/\ \L\.\_ /\ \L\ \\ \ \ /\ \/\ \ /\ \L\.\_ /\ \L\ \ /\ \L\ \\ \ \/ 
+	 \ \ ,__/\ \__/.\_\\ \____ \\ \_\\ \_\ \_\\ \__/.\_\\ \___,_\\ \____/ \ \_\ 
+	  \ \ \/  \/__/\/_/ \/___L\ \\/_/ \/_/\/_/ \/__/\/_/ \/__,_ / \/___/   \/_/ 
+	   \ \_\              /\____/                                               
+	    \/_/              \_/__/                                
 
 
 
-   Paginador de resultats
+	   Paginador de resultats
 
-   Última modificació # Dídac # 2012-09-20 10:48:28
+	   Última modificació # Dídac # 2012-11-15 09:53:49
 
-   		$pagina = new ts_paginador('ofertes'); 			// creem nou objecte, indicant la taula amb la que treballarem (ha d'haver una connexió mysql oberta)
+	   # Dídac # 2012-11-15 09:54:46 # nova funcio: ts_sel()
 
-		$pagina->ts_where($where); 			// si hi ha alguna condicio (buscador, etc..)
-		$pagina->ts_order('id DESC'); 			// ordenació 
-		$pagina->estableix_resultats_pagina(10); 	// * Establim els resultats per pàgina, per defecte 30 
-		$pagina->estableix_varpagina('pagina'); 	// * Establim la variable de la pagina web.php?VARIABLE=1|2|3|4|5|N
-		$the_query = $pagina->fem_query(); 		// Fem la consulta
+	   		$pagina = new ts_paginador('ofertes'); 		// creem nou objecte, indicant la taula amb la que treballarem (ha d'haver una connexió mysql oberta)
 
-		$pagina->mostrar_links(7);			// Mostrem els links
+			$pagina->ts_sel('camp1, camp2');			// podem seleccionar, per defecte fa SELECT * ara podem definir el *
+			$pagina->ts_where($where); 					// si hi ha alguna condicio (buscador, etc..)
+			$pagina->ts_order('id DESC'); 				// ordenació 
+			$pagina->estableix_resultats_pagina(10); 	// * Establim els resultats per pàgina, per defecte 30 
+			$pagina->estableix_varpagina('pagina'); 	// * Establim la variable de la pagina web.php?VARIABLE=1|2|3|4|5|N
+			$the_query = $pagina->fem_query(); 			// Fem la consulta
 
-		* Les instancies amb asterisc (*) no són necessaries, només les cridarem en cas de voler canviar els valors x defecte
+			$pagina->mostrar_links(7);					// Mostrem els links
 
-   
-   ========================================================================== */
+			* Les instancies amb asterisc (*) no són necessaries, només les cridarem en cas de voler canviar els valors x defecte
 
-
-class ts_paginador {
-
-	/* definim variables que s'utilitzaran */
-
-	var $n_pag = 1; 		// Nombre de pagina actual, per defecte serà 1 
-	var $rpp = 30; 			// registres per pagina, per defecte 30
-	var $url;			// la pàgina en la que estem, per crear els links correctament
-	var $total_registres;		// total de registres de la consulta realitzada
-	var $taula;			// la taula amb la que treballarem
-	var $numero_pagines;		// numero de pàgines totals que hi haurà
-	var $pvar = 'p'; 		// la variable que indicarà el numero de pàgina, per defecte es p ($_GET['p'])
-	var $where;			// where
-	var $order;			// order by
+	   
+	   ========================================================================== */
 
 
-	function ts_paginador($ts_taula) {
-		global $n_pag;
-		$this->taula=$ts_taula;
-	}
+	class ts_paginador {
+
+		/* definim variables que s'utilitzaran */
+
+		var $n_pag = 1; 		// Nombre de pagina actual, per defecte serà 1 
+		var $rpp = 30; 			// registres per pagina, per defecte 30
+		var $url;				// la pàgina en la que estem, per crear els links correctament
+		var $total_registres;	// total de registres de la consulta realitzada
+		var $taula;				// la taula amb la que treballarem
+		var $numero_pagines;	// numero de pàgines totals que hi haurà
+		var $pvar = 'p'; 		// la variable que indicarà el numero de pàgina, per defecte es p ($_GET['p'])
+		var $sel = '*'			// per defecte seleccionem tot *
+		var $where;				// where
+		var $order;				// order by
 
 
-	function ts_where($ts_condicio) {
-		$where_pla = count( $ts_condicio ) ? "\nWHERE " . implode( ' AND ', $ts_condicio ) : '';		
-		$this->where=$where_pla;
-	}
-
-	function ts_order($ts_order) {
-		$this->order=$ts_order;
-	}
-
-	function total_registres() {
-
-		$query="SELECT COUNT(*) AS total FROM ".$this->taula;
-
-		if ($this->where!="") {
-			 $query .= $this->where;
+		function ts_paginador($ts_taula) {
+			global $n_pag;
+			$this->taula=$ts_taula;
 		}
 
-		$result=mysql_query($query) or die('L73:'. mysql_error());
 
-		$row=mysql_fetch_object($result);
-		$this->total_registres= $row->total;
-
-	}
-
-	function pagina_actual() {
-	 if (isset($_GET[$this->pvar])) { $pagina_actual = $_GET[$this->pvar]; } else { $pagina_actual = '1'; } // pàgina actual
-	 return($pagina_actual);
-	}
-
-		// numeros de pagines totals
-	function pagines_totals() {
-
-		$this->total_registres();
-
-		$this->numeros_pagines = (int)($this->total_registres / $this->rpp); // pagines totals que hi haurà
-		if(($this->total_registres%$this->rpp) != 0) { $this->numeros_pagines++; }
-
-	}
-
-	function estableix_resultats_pagina($num) {
-		$this->rpp=$num;
-		$this->pagines_totals();
-	}
-
-	function estableix_varpagina($str) {
-		$this->pvar = $str;
-	}
-
-
-	function obtenim_url() {
-
-		global $_GET;
-
-		if (!empty($_SERVER['SCRIPT_URL'])) { 
-			$la_pagina = $_SERVER['SCRIPT_URL'];
-		} else {
-			$la_pagina = $_SERVER['REDIRECT_URL'];
+		function ts_where($ts_condicio) {
+			$where_pla = count( $ts_condicio ) ? "\nWHERE " . implode( ' AND ', $ts_condicio ) : '';		
+			$this->where=$where_pla;
 		}
 
-		while (list ($clave, $val) = each ($_GET)) {
-			if($clave != $this->pvar) {
-				 $variables .= $clave."=".$val."&";
+		function ts_order($ts_order) {
+			$this->order=$ts_order;
+		}
+
+		function ts_sel($ts_sel) {
+			$this->sel=$ts_sel;
+		}
+
+		function total_registres() {
+
+			$query="SELECT COUNT(*) AS total FROM ".$this->taula;
+
+			if ($this->where!="") {
+				 $query .= $this->where;
 			}
+
+			$result=mysql_query($query) or die('L73:'. mysql_error());
+
+			$row=mysql_fetch_object($result);
+			$this->total_registres= $row->total;
+
 		}
 
-		 $this->url = $la_pagina."?".$variables;
-	}	
+		function pagina_actual() {
+		 if (isset($_GET[$this->pvar])) { $pagina_actual = $_GET[$this->pvar]; } else { $pagina_actual = '1'; } // pàgina actual
+		 return($pagina_actual);
+		}
 
-	// retorna $result amb el query ja paginat
-	function fem_query() {
+			// numeros de pagines totals
+		function pagines_totals() {
 
-		if (isset($_GET[$this->pvar])) {
-			$this->n_pag=$_GET[$this->pvar];
-		} else {
-			$this->n_pag=1;
-		}		
+			$this->total_registres();
 
-		$query = "SELECT * FROM ". $this->taula;
+			$this->numeros_pagines = (int)($this->total_registres / $this->rpp); // pagines totals que hi haurà
+			if(($this->total_registres%$this->rpp) != 0) { $this->numeros_pagines++; }
 
-		if ($this->where != "") { $query .= $this->where; }
-		if($this->order != "") { $query .= " ORDER BY ". $this->order; }
+		}
 
-		$limitacio = ($this->n_pag-1) * $this->rpp;
+		function estableix_resultats_pagina($num) {
+			$this->rpp=$num;
+			$this->pagines_totals();
+		}
 
-		$query .= " LIMIT ".$limitacio.",".$this->rpp;
-
-		$result = mysql_query($query) or die('L143:'. mysql_error());
-
-		//echo($query);
-
-		return($result);
-
-	}
+		function estableix_varpagina($str) {
+			$this->pvar = $str;
+		}
 
 
-	function mostrar_links($mostrarmax=7) {
+		function obtenim_url() {
 
-		$pagina_actual = $this->pagina_actual();
-		$this->pagines_totals();
-		$pagtotals = $this->numeros_pagines;
+			global $_GET;
 
-		if (!$this->url) { $this->obtenim_url(); }
+			if (!empty($_SERVER['SCRIPT_URL'])) { 
+				$la_pagina = $_SERVER['SCRIPT_URL'];
+			} else {
+				$la_pagina = $_SERVER['REDIRECT_URL'];
+			}
 
-		if ($pagtotals >= 1) {
+			while (list ($clave, $val) = each ($_GET)) {
+				if($clave != $this->pvar) {
+					 $variables .= $clave."=".$val."&";
+				}
+			}
 
-			echo '<nav class="ts_paginacio">';
+			 $this->url = $la_pagina."?".$variables;
+		}	
 
-			if ($pagina_actual == 1) {
-				echo '
+		// retorna $result amb el query ja paginat
+		function fem_query() {
+
+			if (isset($_GET[$this->pvar])) {
+				$this->n_pag=$_GET[$this->pvar];
+			} else {
+				$this->n_pag=1;
+			}		
+
+			$query = "SELECT ".$this->sel." FROM ". $this->taula;
+
+			if ($this->where != "") { $query .= $this->where; }
+			if($this->order != "") { $query .= " ORDER BY ". $this->order; }
+
+			$limitacio = ($this->n_pag-1) * $this->rpp;
+
+			$query .= " LIMIT ".$limitacio.",".$this->rpp;
+
+			$result = mysql_query($query) or die('L143:'. mysql_error());
+
+			//echo($query);
+
+			return($result);
+
+		}
+
+
+		function mostrar_links($mostrarmax=7) {
+
+			$pagina_actual = $this->pagina_actual();
+			$this->pagines_totals();
+			$pagtotals = $this->numeros_pagines;
+
+			if (!$this->url) { $this->obtenim_url(); }
+
+			if ($pagtotals >= 1) {
+
+				echo '<nav class="ts_paginacio">';
+
+				if ($pagina_actual == 1) {
+					echo '
+						<span class="inactiu">
+							«
+						</span>				
+					';
+				} else {
+					echo '
+						<a href="'.$this->url.'" class="paginar" title="Primera" data-numpag="1">
+							1
+						</a>		
+						<a href="'.$this->url.''.$this->pvar.'='.($pagina_actual-1).'" class="paginar" title="Anterior"  data-numpag="'.($pagina_actual-1).'">
+							«
+						</a>				
+					';
+				}
+
+				$pap5 = $pagina_actual + $mostrarmax; // pagina actual + LES QUE VOLGUEM
+				$pam5 = $pagina_actual - $mostrarmax; // pagina actual - LES QUE VOLGUEM
+
+
+				if (($pagina_actual + $mostrarmax) < $pagtotals && ($pagina_actual - $mostrarmax) >= 1) { // 1
+
+					$show_pag = '<span>...</span>';
+
+					for ($tvar=$pam5;$tvar<=$pap5;$tvar++) {
+						if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }
+						$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" class="paginar'.$activeono.'" data-numpag="'.$tvar.'">'.$tvar.'</a>';
+					}
+
+					if ($pap5 != $pagtotals) { $show_pag = $show_pag.'<span>...</span>'; }
+
+				} elseif (($pagina_actual + $mostrarmax) >= $pagtotals && ($pagina_actual - $mostrarmax) > 1) { //2
+
+					$show_pag = '<span>...</span>';
+
+					for ($tvar=$pam5;$tvar<=$pagtotals;$tvar++) {			
+						if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }			
+						$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" data-numpag="'.$tvar.'" class="paginar'.$activeono.'">'.$tvar.'</a>';
+					}
+
+					if ($pap5 == $pagtotals) { $show_pag = $show_pag.'<span>...</span>'; }
+
+
+				} elseif (($pagina_actual + $mostrarmax) < $pagtotals && ($pagina_actual - $mostrarmax) < 1) { //3
+
+					for ($tvar=1;$tvar<=$pap5;$tvar++) {
+						if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }			
+						$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" data-numpag="'.$tvar.'" class="paginar'.$activeono.'">'.$tvar.'</a>';
+					}
+					$show_pag = $show_pag.'<span>...</span>';
+
+				} else { //4
+					for ($tvar=1;$tvar<=$pagtotals;$tvar++) {
+						if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }
+						$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" data-numpag="'.$tvar.'" class="paginar'.$activeono.'">'.$tvar.'</a>';
+					}
+				}			
+
+
+				echo $show_pag; // mostrem el intermig de numeros
+
+
+				if ($pagina_actual == $pagtotals) {
+					echo '
 					<span class="inactiu">
-						«
-					</span>				
-				';
-			} else {
-				echo '
-					<a href="'.$this->url.'" class="paginar" title="Primera" data-numpag="1">
-						1
-					</a>		
-					<a href="'.$this->url.''.$this->pvar.'='.($pagina_actual-1).'" class="paginar" title="Anterior"  data-numpag="'.($pagina_actual-1).'">
-						«
-					</a>				
-				';
+						»
+					</span>
+					';				
+				} else {
+					echo '			
+						<a href="'.$this->url.''.$this->pvar.'='.($pagina_actual+1).'" data-numpag="'.($pagina_actual+1).'" class="paginar" title="Següent">»</a>				
+						<a href="'.$this->url.''.$this->pvar.'='.$pagtotals.'" data-numpag="'.$pagtotals.'" class="paginar" title="Última">'.$pagtotals.'</a>
+					';				
+				}			
+
+				echo "</nav>";
 			}
 
-			$pap5 = $pagina_actual + $mostrarmax; // pagina actual + LES QUE VOLGUEM
-			$pam5 = $pagina_actual - $mostrarmax; // pagina actual - LES QUE VOLGUEM
+		}	
 
-
-			if (($pagina_actual + $mostrarmax) < $pagtotals && ($pagina_actual - $mostrarmax) >= 1) { // 1
-
-				$show_pag = '<span>...</span>';
-
-				for ($tvar=$pam5;$tvar<=$pap5;$tvar++) {
-					if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }
-					$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" class="paginar'.$activeono.'" data-numpag="'.$tvar.'">'.$tvar.'</a>';
-				}
-
-				if ($pap5 != $pagtotals) { $show_pag = $show_pag.'<span>...</span>'; }
-
-			} elseif (($pagina_actual + $mostrarmax) >= $pagtotals && ($pagina_actual - $mostrarmax) > 1) { //2
-
-				$show_pag = '<span>...</span>';
-
-				for ($tvar=$pam5;$tvar<=$pagtotals;$tvar++) {			
-					if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }			
-					$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" data-numpag="'.$tvar.'" class="paginar'.$activeono.'">'.$tvar.'</a>';
-				}
-
-				if ($pap5 == $pagtotals) { $show_pag = $show_pag.'<span>...</span>'; }
-
-
-			} elseif (($pagina_actual + $mostrarmax) < $pagtotals && ($pagina_actual - $mostrarmax) < 1) { //3
-
-				for ($tvar=1;$tvar<=$pap5;$tvar++) {
-					if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }			
-					$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" data-numpag="'.$tvar.'" class="paginar'.$activeono.'">'.$tvar.'</a>';
-				}
-				$show_pag = $show_pag.'<span>...</span>';
-
-			} else { //4
-				for ($tvar=1;$tvar<=$pagtotals;$tvar++) {
-					if ($pagina_actual == $tvar) { $activeono = ' active'; } else { $activeono=''; }
-					$show_pag = $show_pag.'<a href="'.$this->url.''.$this->pvar.'='.$tvar.'" data-numpag="'.$tvar.'" class="paginar'.$activeono.'">'.$tvar.'</a>';
-				}
-			}			
-
-
-			echo $show_pag; // mostrem el intermig de numeros
-
-
-			if ($pagina_actual == $pagtotals) {
-				echo '
-				<span class="inactiu">
-					»
-				</span>
-				';				
-			} else {
-				echo '			
-					<a href="'.$this->url.''.$this->pvar.'='.($pagina_actual+1).'" data-numpag="'.($pagina_actual+1).'" class="paginar" title="Següent">»</a>				
-					<a href="'.$this->url.''.$this->pvar.'='.$pagtotals.'" data-numpag="'.$pagtotals.'" class="paginar" title="Última">'.$pagtotals.'</a>
-				';				
-			}			
-
-			echo "</nav>";
-		}
-
-	}	
-
-}
+	}
 ?>
